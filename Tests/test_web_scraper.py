52ff848971db62a257b95fcb4674282b1228715b
dustykeyboard1 <mikesyanks02@icloud.com>
2023-10-12 13:59:29 -0400

    Quarterback stats

--- Tests/test_web_scraper.py
+++ Tests/test_web_scraper.py
@@ -11,11 +11,11 @@ sys.path.append(root_dir)
 from WebScraper.web_scraper import WebScraper
 
 def main():
-    scraper = WebScraper(team1="Miami Dolphins", team2="New York Giants")
+    scraper = WebScraper(team1="Buffalo Bills", team2="New York Jets", qb1='Josh Allen', qb2='Zach Wilson')
     scraper.fetch_data()
     scraper.parse_data()
     scraper.save_data()
-    scraper.print_team_data()
+    # scraper.print_team_data()
 
 
 
