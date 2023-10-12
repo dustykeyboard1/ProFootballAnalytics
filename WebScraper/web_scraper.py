1672da98ad783dcfec1825a91a324432a39da72b
dustykeyboard1 <mikesyanks02@icloud.com>
2023-10-12 13:14:20 -0400

    Basic setup

--- WebScraper/web_scraper.py
+++ WebScraper/web_scraper.py
@@ -9,9 +9,10 @@ the ProFootballReference website.
 
 import requests
 import json 
+from bs4 import BeautifulSoup
 
 class WebScraper:
-    def __init__(self, config_file='config.json'):
+    def __init__(self, team1, team2, config_file='WebScraper/config.json'):
         '''
         Initialize a new WebScraper
         Parameters: config_file - saved in the WebScraper Directory
@@ -22,21 +23,37 @@ class WebScraper:
         self.headers = config['headers']
         self.raw_data = None
         self.parsed_data = None
-    
-    def set_teams(self, team1, team2):
         self.team1 = team1
         self.team2 = team2
-        self.team1_url = f"{self.base_url}/{self.team1}"
-        self.team2_url = f"{self.base_url}/{self.team2}"
+    
+    def set_teams(self):
+        self.team1_url = f"{self.base_url}{self.team1}/2023.htm"
+        self.team2_url = f"{self.base_url}{self.team2}/2023.htm"
 
     def fetch_data(self):
-        self.raw_data = {}
-        for team, url in {'team1': self.team1_url, 'team2': self.team2_url}.items():
-            response = requests.get(url, headers=self.headers)
-            if response.status_code == 200:
-                self.raw_data[team] = response.text
-            else:
-                print(f"Failed to fetch data for {team}. Status code: {response.status_code}")
+        self.team1_data = self._fetch_team_data(self.team1_url)
+        self.team2_data = self._fetch_team_data(self.team2_url)
+
+    def _fetch_team_data(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Scrape Schedule & Game Results Table
+        schedule_table = soup.find('table', {'id': 'games'})
+        # Your code to scrape this table
+        print(schedule_table)
+        
+        # Scrape Passing Table
+        passing_table = soup.find('table', {'id': 'passing'})
+        # Your code to scrape this table
+        
+        # Scrape Rushing & Receiving Table
+        rushing_table = soup.find('table', {'id': 'rushing_and_receiving'})
+        # Your code to scrape this table
+        
+        # Scrape Defense & Fumbles Table
+        defense_table = soup.find('table', {'id': 'defense'})
+        # Your code to scrape this table
 
 
     def parse_data(self):
@@ -45,3 +62,7 @@ class WebScraper:
     def save_data(self, file_name='temp_data.json'):
         with open(file_name, 'w') as f:
             json.dump(self.parsed_data, f)
+
+    def print_team_urls(self):
+        print(self.team1_url)
+        print(self.team2_url)
--- main.py
+++ main.py
@@ -4,5 +4,9 @@ Date: 10-12-2023
 Functionality: Run the product
 '''
 
-from WebScraper import WebScraper
+from WebScraper.web_scraper import WebScraper
 
+def main():
+    scraper = WebScraper()
+    scraper.set_teams('NY/2023.htm', 'MIA/2023.htm')
+    scraper.fetch_data()
